20d26dd5be5aec096cc615cda89d166cee87cc61
Daniel <routybor@gmail.com>
2025-09-29 11:36:54 +0200

    Changed models

--- adminpanel/settings.py
+++ adminpanel/settings.py
@@ -3,92 +3,102 @@ from os import getenv
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
-SECRET_KEY = getenv('SECRET_KEY')
+
+
+class CONSTANTS:
+    FIELD_LEN = 255
+    TITLE_CHOICES = (
+        (r"Gaming computer", r"Игровой компьютер"),
+        (r"Office computer", r"Рабочий компьютер"),
+    )
+
+
+SECRET_KEY = getenv("SECRET_KEY")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'panel',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "panel",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'adminpanel.urls'
+ROOT_URLCONF = "adminpanel.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'adminpanel.wsgi.application'
+WSGI_APPLICATION = "adminpanel.wsgi.application"
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': getenv('DB_NAME'),
-        'USER': getenv('DB_USER'),
-        'PASSWORD': getenv('DB_PASSWORD'),
-        'HOST': getenv('DB_HOST'),
-        'PORT': getenv('DB_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("DB_NAME"),
+        "USER": getenv("DB_USER"),
+        "PASSWORD": getenv("DB_PASSWORD"),
+        "HOST": getenv("DB_HOST"),
+        "PORT": getenv("DB_PORT"),
     }
 }
 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = "ru-RU"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- panel/models.py
+++ panel/models.py
@@ -1,15 +1,49 @@
 from django.db import models
+from django.conf import settings
+
+
+CON = settings.CONSTANTS
+
 
 class Computer(models.Model):
-    title = models.CharField("Название", max_length=255)
-    link = models.TextField("Ссылка", blank=True, null=True)
-    price = models.DecimalField("Цена", max_digits=10, decimal_places=2, blank=True, null=True)
-    cpu = models.CharField("Процессор", max_length=255, blank=True, null=True)
-    gpu = models.CharField("Видеокарта", max_length=255, blank=True, null=True)
-    ram = models.CharField("Оперативная память", max_length=255, blank=True, null=True)
-    motherboard = models.CharField("Материнская плата", max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
-    photo = models.BinaryField("Фото", blank=True, null=True, editable=True)
+    title = models.TextField(
+        verbose_name="Название",
+        max_length=CON.FIELD_LEN,
+        choices=CON.TITLE_CHOICES,
+        default=CON.TITLE_CHOICES[0][0],
+    )
+    link = models.TextField(
+        verbose_name="Ссылка",
+    )
+    price = models.DecimalField(
+        verbose_name="Цена",
+        max_digits=10,
+        decimal_places=2,
+    )
+    cpu = models.CharField(
+        verbose_name="Процессор",
+        max_length=CON.FIELD_LEN,
+    )
+    gpu = models.CharField(
+        verbose_name="Видеокарта",
+        max_length=CON.FIELD_LEN,
+    )
+    ram = models.CharField(
+        verbose_name="Оперативная память",
+        max_length=CON.FIELD_LEN,
+    )
+    motherboard = models.CharField(
+        verbose_name="Материнская плата",
+        max_length=CON.FIELD_LEN,
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Дата создания",
+        auto_now_add=True,
+    )
+    photo = models.BinaryField(
+        verbose_name="Фото",
+        editable=True,
+    )
 
     class Meta:
         verbose_name = "Компьютер"
